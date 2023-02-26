7f574d49033d90cf0796eacdaf6fc835f9e4c6d0
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2023-02-26 10:14:08 +0300

    reservationserializer

--- car/serializers.py
+++ car/serializers.py
@@ -24,3 +24,23 @@ class CarSerializer(serializers.ModelSerializer):
             fields.pop("plate_number") 
         
         return fields   
+    
+class ReservationSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model=Reservation
+        fields=(
+            "id",
+            "customer",
+            "car",
+            "start_date",
+            "end_date",
+        )  
+        
+        validators=[  #gönderilen bütün datanın hepsini database e gelmeden kontrol ediyor.
+            serializers.UniqueTogetherValidator(
+                queryset=Reservation.objects.all(),
+                fields=("customer","start_date","end_date"),
+                message=("You already have a reservation between these dates...")
+            )
+        ]  
