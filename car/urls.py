ff12bca7beb001ff6d7677e16d8b47f950f2f89d
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2023-02-26 12:24:22 +0300

    reservation2

--- car/urls.py
+++ car/urls.py
@@ -2,14 +2,14 @@ from django.urls import path
 
 from rest_framework import routers
 
-from .views import CarView
+from .views import CarView,ReservationView
 
 router=routers.DefaultRouter()
 
 router.register("car", CarView)
 
 urlpatterns = [
-    
+    path("reservation/",ReservationView.as_view()),
 ]
 
 urlpatterns+=router.urls
